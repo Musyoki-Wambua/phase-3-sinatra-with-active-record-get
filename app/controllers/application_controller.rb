091e817dfc3ca8b5150f8504faee36d9471e0af7
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-02-27 11:48:59 +0300

    Completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,24 @@
 class ApplicationController < Sinatra::Base
+  set  :default_content_type, 'application/json'
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  get '/games' do
+    games = Game.all.order(:title).limit(10)
+    games.to_json(include: :reviews)
+  end
+
+  get '/games/:id' do 
+    game = Game.find(params[:id])
+    game.to_json(only: [:id, :title, :genre, :price], 
+      include: {
+        reviews: {
+          only: [:score, :comment],
+          include: {
+            user: {
+              only: [:name]
+            }
+          }
+        }
+      })
   end
 
 end
